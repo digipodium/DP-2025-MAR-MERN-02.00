5822ece6dbbad4047dfdaa4fb5754d52f7cb063e
Ayushman Gupta <ayushman0021@gmail.com>
2025-04-28 15:47:06 +0530

    Add global styles and layout for Next.js application
    - Created globals.css for defining root styles and dark mode support.
    - Implemented layout.jsx to set up the root layout with custom fonts using Next.js font optimization.
    - Added a basic page component in page.jsx to serve as the main content area.

--- Section-1/javascript/map-filter.js
+++ Section-1/javascript/map-filter.js
@@ -1,6 +1,6 @@
 const nums = [8, 5, 7, 2, 4, 6, 3, 1, 9];
 
-const sqr = nums.map((n) => {return n * n});
+const sqr = nums.map((n) => { return n * n });
 
 console.log(sqr);
 
@@ -12,11 +12,11 @@ console.log(gst);
 
 const prices2 = ['₹340.41', '₹810.23', '₹1450.80', '₹990.68', '₹120.37'];
 
-const intPrice = prices2.map((p) => { return(p.slice(1) ) });
+const intPrice = prices2.map((p) => { return (p.slice(1)) });
 
 console.log(intPrice);
 
-const even = nums.filter((n) => { return (n%2 == 0) });
+const even = nums.filter((n) => { return (n % 2 == 0) });
 console.log(even);
 
 const greaterThan500 = prices.filter((p) => { return (p >= 500) });
@@ -29,6 +29,27 @@ console.log(upper);
 
 const names = ['Bubli', 'Rinkiya', 'Chinkiya', 'Pinki', 'Kalu']
 
-const longNames = names.filter((n) => {return (n.length > 5)});
+const longNames = names.filter((n) => { return (n.length > 5) });
 console.log(longNames);
+
+const fullNames = ['Vishal Kushwaha', 'Pawan Sharma', 'Chirag Saxena', 'Faizan Ansari', 'Satish Yadav']
+
+const firstNames = fullNames.map((f) => { return f.split(' ')[0] });
+console.log(firstNames);
+
+console.log(fullNames.includes('Vishal Kushwaha'));
+
+const decimals = [2.8, 45.2, 12.5, 7.9, 3.4, 8.1];
+
+//Convert to integers then filter the even numbers
+
+console.log(decimals.map((d) => parseInt(d)).filter((d) => d % 2 == 0));
+
+
+//reduce method = it is used to reduce the array to a single value by applying a function to each element of the array.
+//It takes two arguments: a callback function and an initial value. The callback function takes two arguments: an accumulator and the current value. The initial value is optional and is used as the starting point for the reduction.
+
+const arr = [1,2,3,4,5];
+const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(sum);
 
