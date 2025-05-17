8acaf169f5ef1e1535414f3f1f3e85704563c9ba
Ayushman Gupta <ayushman0021@gmail.com>
2025-05-17 16:31:48 +0530

    fix: update userRouter to handle errors and return JSON responses for getall and getbyid routes

--- Section-3/backend/routers/userRouter.js
+++ Section-3/backend/routers/userRouter.js
@@ -12,13 +12,30 @@ router.post('/add', (req, res) => {
         .then((result) => {
             res.status(200).json(result);
         }).catch((err) => {
+            res.status(500).json(err);
             console.log(err);
         });
 });
 
 router.get('/getall', (req, res) => {
-    res.send('Add Route from userRouter');
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+            console.log(err);
+        });
 });
+
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+            console.log(err);
+        });
+})
 
 
 module.exports = router;
