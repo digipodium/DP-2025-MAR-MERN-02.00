378cb74c5986c5dc08eba5cfe4f52029bb8c76ec
Ayushman Gupta <ayushman0021@gmail.com>
2025-05-17 17:15:17 +0530

    fix: correct getbyid route to use correct parameter and improve error handling

--- Section-3/backend/routers/userRouter.js
+++ Section-3/backend/routers/userRouter.js
@@ -28,14 +28,33 @@ router.get('/getall', (req, res) => {
 });
 
 router.get('/getbyid/:id', (req, res) => {
-    Model.findById(req.params.id)
+    Model.find({ city: req.params.city })
         .then((result) => {
             res.status(200).json(result);
         }).catch((err) => {
             res.status(500).json(err);
             console.log(err);
         });
-})
+});
 
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+            console.log(err);
+        });
+});
+
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+            console.log(err);
+        });
+});
 
 module.exports = router;
